d9414cec681c8cbd35cea57a0248fd8335a90ba3
tpavankumar <30527579+tpavankumar@users.noreply.github.com>
2017-10-03 15:41:37 +0530

    Update post_install.sh

--- deployment_scripts/post_install.sh
+++ deployment_scripts/post_install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-sudo \cp -f /home/ubuntu/flarum/deployment_scripts/flarum.ini /etc/php5/fpm/conf.d/gravity.ini
-sudo \cp -f /home/ubuntu/flarum/deployment_scripts/nginx.conf /etc/nginx/nginx.conf
-cd /home/ubuntu
-#cd /home/ubuntu/flarum
+sudo cp /home/ubuntu/flarum/deployment_scripts/flarum.ini /etc/php5/fpm/conf.d/gravity.ini
+sudo cp /home/ubuntu/flarum/deployment_scripts/nginx.conf /etc/nginx/nginx.conf
+cd /home/ubuntu/flarum
 php composer.phar install
 php flarum cache:clear
